201248c2c10877bccd4d8b2be738e6ff2a04ed4c
Denis Dovzhenko <dovzdenis@gmail.com>
2019-12-13 17:08:23 +0200

    add dotenv

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+require("dotenv").config({ path: "./config/.env" });
+// OR
+// const PORT = require("./config/options.json5").port;
+const PORT = process.env.PORT;
 if (process.env.NODE_ENV === "production") console.log("Server started in production mode");
 else {
-  require("console-stamp")(console, "mm-dd HH:MM:ss");
-  console.log("Server started in dev mode");
+    require("console-stamp")(console, "mm-dd HH:MM:ss");
+    console.log(`Server started in dev mode on port ${PORT}`);
 }
 
-const express = require("express");
+const express = require("express"),
+    app = express();
 const path = require("path");
 const mysql = require("mysql");
 
 // const logger = require("./utils/logger");
 
-const app = express();
 // app.use(logger); // Middleware
 
 // Body parser middleware
@@ -20,27 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 // Enable pug templates
 app.set("view engine", "pug");
 
-// if (contentType == "text/html" && extname == "") filePath += ".html";
-
-// /* GET home page. */
-// app.get("/", (req, res) => {
-//   const userLocale = req.acceptsLanguages()[0];
-//   res.render("index", { title: "Homepage", message: "Homepage", userLocale });
-// });
-// /* GET about page. */
-// app.get("/about", (req, res) => {
-//   res.render("about", { title: "About", message: "About page" });
-// });
-
-// app.get("/info", (req, res) => {
-//   const myURL = new URL("http://example.com/hello.html?id=8f35490242&status=active");
-//   const refID = myURL.searchParams.get("id");
-//   const data = {
-//     refID
-//   };
-//   res.render("info", { title: "Info", data });
-// });
-
 // Set static folder for css,js,imgs & compile less
 app.use(require("less-middleware")(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
@@ -49,34 +32,32 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/index"));
 app.use("/api/users", require("./routes/api/users"));
 
-// Create dynamic port based on server
-const PORT = require("./config/options.json5").port;
-console.log("PORT: ", PORT);
-// const PORT = process.env.PORT || 5000;
+// const PORT = require("./config/options.json5").port;
+// // const PORT = process.env.PORT || 5000;
 
 // Connect to MySQL
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "acme"
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "acme"
 });
 
 db.connect();
 
 app.get("/users", (req, res) => {
-  const sql = "SELECT * FROM users";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.render("users", { title: "Users", result });
-  });
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.render("users", { title: "Users", result });
+    });
 });
 // END MySQL code
 
 // Handle 404 page
 app.use((req, res, next) => {
-  res.status(404);
-  res.render("404", { title: "404" });
+    res.status(404);
+    res.render("404", { title: "404" });
 });
 
 app.listen(PORT);
